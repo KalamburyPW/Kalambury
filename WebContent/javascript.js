387502ab6adb9af83774173faff1a70061547a7e
szabala94 <kalambury.pw@gmail.com>
2020-04-05 12:43:32 +0200

    init commit

---
+++ WebContent/javascript.js
@@ -0,0 +1,32 @@
+var websocket = new WebSocket(
+		"ws://localhost:8080/websockety3/serverendpointdemo");
+websocket.onmessage = function rocessMessage(message) {
+	var jsonData = JSON.parse(message.data);
+	var word = "WORD";
+	var word2 = "abc";
+	var w = jsonData.type;
+	var text = jsonData.txt;
+	var mta = document.getElementById("messagesTextArea");
+
+	if (jsonData.message != null) {
+
+		messagesTextArea.value += jsonData.message + "\n";
+
+		setInterval(mta.scrollTop = mta.scrollHeight);
+
+	}
+	if (w.localeCompare(word) || w.localeCompare(word2) === 0) {
+		randomText.value = text;
+
+	}
+
+}
+
+function nextText() {
+	websocket.send("next");
+}
+
+function sendMessage() {
+	websocket.send(messageText.value);
+	messageText.value = "";
+}
